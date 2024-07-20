052dcb18ade4233ee3d8976fb81e29dac4d759a7
MauricioAires <mauricioaires015@gmail.com>
2024-07-20 13:16:45 -0300

    feat: use node exec

---
+++ exemplo-2/main.js
@@ -0,0 +1,19 @@
+console.log("Testando");
+
+var a = 10;
+var b = 20;
+var total = a * b;
+
+console.log("O resultado da multiplicação é: " + total);
+
+// Armazenar uma informação
+// Letram underline cifrão
+var nomePessoa = "Mauricio";
+console.log(nomePessoa);
+
+var var1 = 1,
+  var2 = 2,
+  var3 = 3;
+
+// O var é global ... mesmo em um escopo local ela é disponível no escopa
+// global .. por isso utilizar apenas let e const
