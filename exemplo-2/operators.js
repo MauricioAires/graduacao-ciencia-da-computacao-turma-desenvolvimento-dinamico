f6c852e3784714808a11b51d5408dfe6d5d23f7c
MauricioAires <mauricioaires015@gmail.com>
2024-07-27 15:37:41 -0300

    feat: operators

---
+++ exemplo-2/operators.js
@@ -0,0 +1,10 @@
+// Verificar se a é maior que b, e b é divisível por 2;
+
+let a = 6;
+let b = 4;
+
+if (a > b && b % 2 === 0) {
+  console.log("a é maior que b e b é divisível por 2");
+} else {
+  console.log("a não é maior que b ou b não é divisível por 2");
+}
